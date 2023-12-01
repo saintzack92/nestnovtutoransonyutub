533f49e6e74ecd9f6e448ce8f318bec209a3580c
Abdullah Chaniago <abdullah.chaniago@ai.astra.co.id>
2023-12-01 16:21:22 +0700

    video 4 finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [CustomersModule],
-  controllers: [],
-  providers: [],
+  imports: [CustomersModule,],
+  controllers: [AppController,],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get } from '@nestjs/common';
+import { CustomersService } from 'src/customers/services/customers/customers.service';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+ constructor (private customersService:CustomersService){    
+ }    
+    @Get('')
+    getCustomer(){
+        return this.customersService.findCustomer()
+    }
+}
