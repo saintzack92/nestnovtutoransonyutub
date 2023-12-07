b3d0ffe43b8b5a108793e5fd83f087f004f39549
saintzack92 <abdullah.chaniago92@gmail.com>
2023-12-07 22:30:05 +0700

    213

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,6 @@
-import { ClassSerializerInterceptor, Controller, Get, HttpException, HttpStatus, Inject, Param, UseInterceptors } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, 
+    HttpException, HttpStatus, Inject, Param, UseInterceptors,ParseIntPipe } from '@nestjs/common';
+import { UserNotFoundException } from 'src/users/exception/UserNotFoundException';
 import { UsersService } from 'src/users/services/users/users.service';
 import { SerializedUser } from 'src/users/types/Index';
 
@@ -22,5 +24,19 @@ export class UsersController {
         }else{
             throw new HttpException('user not found', HttpStatus.BAD_REQUEST)
         }
+    }
+
+    @UseInterceptors(ClassSerializerInterceptor)
+    @Get('id/:id')
+    getByUserId(@Param('id', ParseIntPipe) id:number){
+        console.log('getByUserId works');
+        
+        const user = this.userService.getUserById(id) 
+        if(user)
+          return new SerializedUser(user)
+        else{
+            throw new UserNotFoundException('user not found',HttpStatus.BAD_REQUEST)
+        }
+        
     }
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -6,18 +6,22 @@ import { SerializedUser, User } from 'src/users/types/Index';
 export class UsersService {
     private users:User[]=[
         {
+            id:1,
             username:"andrey",
             password:"andrey",
         },
         {
+            id:2,
             username:"danny",
             password:"danny",
         },
         {
+            id:3,
             username:"stuart",
             password:"stuart",
         },
         {
+            id:4,
             username:"roney",
             password:"roney",
         },
@@ -31,6 +35,12 @@ export class UsersService {
     getUserByUsername(username:string){
         return this.users.find((user)=>{
             return user.username === username
+        })
+    }
+
+    getUserById(id:number){
+        return this.users.find(user=>{
+            return user.id ===id
         })
     }
 }
